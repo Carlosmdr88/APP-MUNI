0cf2af37445f8e04ea9b7b141c9f13bdc4511087
Carlos M Delgado R. <carlosmdr07@hotmail.com>
2026-06-07 20:35:29 -0600

    se realizo la aplicacion par apk

---
+++ ECU CAPACITOR/android/app/src/main/java/com/appmuni/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.appmuni.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ ECU CAPACITOR/www/tailwind-config.js
@@ -0,0 +1,12 @@
+
+      tailwind.config = {
+        theme: {
+          extend: {
+            fontFamily: {
+              sans: ["Plus Jakarta Sans", "sans-serif"],
+              mono: ["JetBrains Mono", "monospace"],
+            },
+          },
+        },
+      };
+    
